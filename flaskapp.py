d2578b652e56326697bb4035f0975595a657871f
Kevin Pearson <krp@cswnet.com>
2019-09-06 15:22:09 -0500

    Added a new dynamic route

--- flaskapp.py
+++ flaskapp.py
@@ -9,10 +9,19 @@ app = Flask(__name__);
 @app.route('/')
 def index():
 	return "<h1>Hello Puppy!</h1>"
+ 
 
+#Add a basic route for infoormation page
 @app.route("/information")
 def info():
 	return "<h1> Puppies are cute</h1"
+
+#add a dynamnic route
+@app.route("/puppy/<name>")	
+def puppy(name):
+	return "<h1> This is a page for {} </h1>".format(name)
+
+
 
 if __name__ == '__main__':
 	app.run()
